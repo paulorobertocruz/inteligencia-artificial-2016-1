7cfa0c37fe3f4f7d9f59f24ae4ec9abf3502b518
paulorobertocruz <paulo.cruz9@gmail.com>
2016-08-24 15:26:47 -0300

    iniciando a implementação da estrutura de dados para manipular o grafo

---
+++ graph.py
@@ -0,0 +1,13 @@
+class Vertex:
+    def __init__(self, label):
+        self.label = label
+
+class Edge:
+    def __init__(self):
+        self.weigth = 0
+
+
+class Graph:
+    def __init__(self):
+        self.verticeis = []
+        self.edges = []
