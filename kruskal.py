ef10a260ac718c85bdcb4ebf73e11f6c1186b2c7
paulorobertocruz <paulo.cruz9@gmail.com>
2016-08-24 15:41:31 -0300

    implementação do disjointsets usando dicionarios e com optmização na união para evitar crescimento da profundidade da arvore sem motivo

--- kruskal.py
+++ kruskal.py
@@ -1,1 +1,20 @@
+class DisjointSets:
+    def __init__(self):
+        self.parents = {}
+        self.sets = {}
+        self.ranks = {}
 
+    def find(self, set):
+        if self.parents[set] == set:
+            return set
+        else:
+            return find(parents[set])
+
+    def union(self, set_a, set_b):
+        if ranks[set_a] > ranks[set_b]:
+            parents[set_a] = set_b
+        elif ranks[set_b] > ranks[set_a]:
+            parents[set_b] = set_a
+        else:
+            parents[set_a] = set_b
+            ranks[set_b]+= 1
