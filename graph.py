c39ac7cfb303cc522cbf0ad96329062c242435dd
paulorobertocruz <paulo.cruz9@gmail.com>
2016-08-24 16:25:47 -0300

    adicioção e arestas e verticies esta completa, a estrutura das arestas esta completa

--- graph.py
+++ graph.py
@@ -1,13 +1,21 @@
-class Vertex:
-    def __init__(self, label):
-        self.label = label
-
 class Edge:
-    def __init__(self):
-        self.weigth = 0
-
+    def __init__(self, weight, vertex_a, vertex_b):
+        self.weigth = weigth
+        self.vertex_a = vertex_a
+        self.vertex_b = vertex_b
 
 class Graph:
     def __init__(self):
         self.verticeis = []
         self.edges = []
+
+    def add_vertex(self, label):
+        if label in self.verticeis:
+            return False
+        else:
+            self.verticeis.append(label)
+            return v_counter
+
+    def add_adge(self, edge):
+        self.edges.append(edge)
+        pass
