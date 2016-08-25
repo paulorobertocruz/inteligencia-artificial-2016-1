a28ef3506f9057a1384df42b16bee765be08bf18
paulorobertocruz <paulo.cruz9@gmail.com>
2016-08-25 17:15:30 -0300

    Pop implementado na classe Lista, melhorado o construtor, modificado o nome do metodo sort para sorted e criado o atributo sorte para verificar se esta lista é ordenada sempre no insert. Implementação inicial do a_star. Impelmentação dos metodos inlist e get_node_from_vertex

--- a_star.py
+++ a_star.py
@@ -1,43 +1,52 @@
 #arquivo proncipal do projeto
 
-from kruskal import Kruskal
-from graph import Graph
-from graph import Edge
+# from kruskal import Kruskal
+# from graph import Graph
+# from graph import Edge
 
 class Node(object):
-    def __init__(self, v):
+    def __init__(self, v, parent=None):
         self.vertex = v
         self.g = 0
         self.h = 0
+        self.parent = parent
 
     def get_f(self):
         return self.g + self.h
-    def set_g(self):
-        pass
-    def set_h(self):
-        pass
 
 class Lista(object):
 
-    def __init__(self):
+    def __init__(self, sort = False):
         self.lista = []
-
+        self.sort = sort
     def pop(self):
-        pass
+        return self.lista.pop(0)
 
-    def insert(self, node, sort = False):
+    def empty(self):
+        return not self.lista
+
+    def insert(self, node):
         #ordena no final?
         self.lista.append(node)
 
-        if sort ==  True:
-            self.sort()
+        if self.sort:
+            self.sorted()
 
-    def sort(self):
+    def sorted(self):
         #define o elemento a ser usado para ordenação da lista
         def order_element(n):
             return n.get_f()
 
         self.lista = sorted(self.lista, key=order_element)
+
+    def inlist(self, node):
+        for l in self.lista:
+            if node.vertex == l.vertex:
+                return True
+        return False
+
+    def get_node_from_vertex(self):
+        return 0
 
     def print_all(self):
         for i in self.lista:
@@ -45,8 +54,53 @@ class Lista(object):
 
 
 def a_star(graph_g, from_node):
-    lista_aberta = Lista()
-    lista_fechada = Lista()
+
+    def g(node, ec):
+        if node.parent is None:
+            return ec
+        else:
+            return node.parent.g + ec
+
+
+    def h():
+        return 0
+
+    lista_aberta = Lista(True)
+
+    lista_fechada = Lista(False)
+
+    from_node.g = g(from_node, 0)
+    from_node.h = h()
+
+    lista_aberta.insert(from_node)
+
+    while(lista_aberta.empty() == False):
+
+        n = lista_aberta.pop()
+
+        edges = graph_g.get_connected_to(n.vertex)
+
+        for e in edges:
+            edge_cost = 0
+            if e.vertex_a == n.vertex:
+                nnode = Node(e.vertex_b, n)
+                edge_cost = e.weigth
+            elif e.vertex_b == n.vertex:
+                edge_cost = e.weigth
+                nnode = Node(e.vertex_a, n)
+
+            # se estiver na lista fechada não sera computado novamente
+            if lista_fechada.inlist(nnode) == False:
+                # se estiver na lista aberta podera ser reparenteado
+                if lista_aberta.inlist(nnode):
+                    if nnode.get_f() < lista_fechada.get_node_from_vertex():
+                        pass
+                else:
+                    nnode.g = g(nnode, edge_cost)
+                    nnode.h = h()
+                    lista_aberta.insert(nnode)
+        lista_fechada.insert(n)
+    return lista_fechada
 
 
 #
